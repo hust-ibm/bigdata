2392caa52a5dfefd0e490eab798dbe1c3b18b269
tigeryoyo <m15527081139@163.com>
2017-03-15 14:04:35 +0800

    创建大数据实验(bigdata)项目

---
+++ src/main/java/com/hust/algorithm/bayes/NativeBayes.java
@@ -0,0 +1,15 @@
+package com.hust.algorithm.bayes;
+
+/**
+ * 朴素贝叶斯分类
+ * @author Chan
+ *
+ */
+public class NativeBayes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/hust/algorithm/kmeans/KMeans.java
@@ -0,0 +1,5 @@
+package com.hust.algorithm.kmeans;
+
+public class KMeans {
+
+}
