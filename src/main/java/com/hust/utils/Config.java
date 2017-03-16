e44730ed32e4967981873f249bef2a702c275c6c
466413559 <466413559@qq.com>
2017-03-16 16:04:42 +0800

    添加用户自定义可配置功能

--- src/main/java/com/hust/utils/Config.java
+++ src/main/java/com/hust/utils/Config.java
@@ -5,7 +5,8 @@ public class Config {
 //	private static String bacePath = System.getProperty("user.dir");
 	//停用词文件路径
 	public static String stopWordPath = "src/main/java/properties/stopwords.txt";
-	
+	//用户词表的存放路径
+	public static String userWordsPath = "src/main/java/properties/userwords.txt";
 	//Canopy聚类结果文件路径
 	public static String CANOPY_RESULT_PATH = "result/cluster/canopy/";
 	//KMeans聚类结果文件路径
